865f2784edcf21fff97ed63765e85e4f2aa6a4fe
César Aarón Fernández Niño <59399364+cesar1091@users.noreply.github.com>
2022-10-19 23:14:59 -0500

    cambio aaron

--- ejercicio_01-1.py
+++ ejercicio_01-1.py
@@ -1,20 +1,53 @@
 def desencriptando_mensajes(horas, mensaje_desencriptado):
-  
-  def rotar(x,horas):
-    horas=-1*horas
-    if (x>=97) & (x<=122):
-      c=x+(horas)%(122-97+1)
-      c=97+(c-97)%(122-97+1)
-    else:
-      #d={ord('$'):ord('.'),ord('.'):ord('$'),ord('&'):ord(','),ord(','):ord('&'),ord('/'):ord(' '),ord(' '):ord('/')}
-      desencriptar={ord('$'):ord('.'),ord('&'):ord(','),ord('/'):ord(' ')}
-      c=desencriptar.get(x,x)      
-    return chr(c)
 
-  def encriptar(horas,mensaje_desencriptado):
-    return ''.join([rotar(ord(i),horas) for i in list(mensaje_desencriptado.lower())])
+  message = mensaje_desencriptado.lower()
+  message_final = ""
+  horas = -1*horas
+  for i in message:
+    if (ord(i)>=97) & (ord(i)<=122):
+      c = x + (horas)%(122-97+1)
+      c = 97 + (c-97)%(122-97+1)
+    else:
+      desencriptar = {ord('$'):ord('.'),
+                      ord('&'):ord(','),
+                      ord(','):ord('&'),
+                      ord('/'):ord(' '),
+                      ord(' '):ord('/')}
+      c = desencriptar.get(ord(i),ord(i))
+    message_final.join(chr(c))
+  return message_final
+
+
+
+
+a=int(input("horas:"))
+b=str(input("mensaje_encriptado:"))
+
+desencriptando_mensajes(a, b)
+
   
-  a=int(input("horas:"))
-  b=str(input("mensaje_encriptado:"))
+  #def rotar(x,horas):
+  #  horas=-1*horas
+  #  if (x>=97) & (x<=122):
+  #    c=x+(horas)%(122-97+1)
+  #    c=97+(c-97)%(122-97+1)
+  #  else:
+      #d={ord('$'):ord('.'),ord('.'):ord('$'),ord('&'):ord(','),ord(','):ord('&'),ord('/'):ord(' '),ord(' '):ord('/')}
+  #    desencriptar={ord('$'):ord('.'),ord('&'):ord(','),ord('/'):ord(' ')}
+  #    c=desencriptar.get(x,x)      
+  #  return chr(c)
+
+  #def encriptar(horas,mensaje_desencriptado):
+  #  return ''.join([rotar(ord(i),horas) for i in list(mensaje_desencriptado.lower())])
   
+  #a=int(input("horas:"))
+  #b=str(input("mensaje_encriptado:"))
+  
+
+
+
+
+
+
+
   return encriptar(a, b)
